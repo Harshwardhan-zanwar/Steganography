71c5340e4177e0a04db3fc8f00c7898d13bbcad2
Harshwardhan-zanwar <156914769+Harshwardhan-zanwar@users.noreply.github.com>
2025-02-21 18:24:46 +0530

    Add files via upload

---
+++ stego.py
@@ -0,0 +1,25 @@
+import cv2
+import os
+import string
+
+img = cv2.imread("Sample_image.jpg")
+password="12345"
+msg="This message is securely hidden"
+
+c = {}
+
+message = ""
+n = 0
+m = 0
+z = 0
+
+pas = input("Enter passcode for Decryption")
+if password == pas:
+    for i in range(len(msg)):
+        message = message + c[img[n, m, z]]
+        n = n + 1
+        m = m + 1
+        z = (z + 1) % 3
+    print("Decryption message:", message)
+else:
+    print("YOU ARE NOT auth")
